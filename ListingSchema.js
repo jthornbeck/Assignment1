7a2300c4b9b306d79e858cf52ec4a6c7bad8c112
Jeff Hornbeck <jthornbeck@ufl.edu>
2017-09-05 14:52:22 -0400

    passed first two cases

--- ListingSchema.js
+++ ListingSchema.js
@@ -1,15 +1,33 @@
 /* Import mongoose and define any variables needed to create the schema */
-var mongoose = require('mongoose'), 
+var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 /* Create your schema */
 var listingSchema = new Schema({
   /* your code here */
+      code: String,
+      name: String,
+      coordinates: {
+          latitude: Number,
+          longitude: Number
+      },
+      address: String,
+      updated_at: Date,
+      created_at: Date
 });
 
 /* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
 listingSchema.pre('save', function(next) {
   /* your code here */
+  var currentDate = new Date();
+  this.updated_at = currentDate;
+  if (!this.created_at) this.created_at=currentDate;
+
+  if (!this.name) throw err;
+  if (!this.code) throw err;
+  next();
+
+
 });
 
 /* Use your schema to instantiate a Mongoose model */
